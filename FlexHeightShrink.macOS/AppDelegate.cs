fbc802ee261477e229c703ea3fc4e43f51cf5c6a
Yusuke Yamada <yamachu.dev@gmail.com>
2018-02-23 01:30:48 +0900

    Initial commit

---
+++ FlexHeightShrink.macOS/AppDelegate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace FlexHeightShrink.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow _window;
+
+        public AppDelegate()
+        {
+            ObjCRuntime.Runtime.MarshalManagedException += (sender, args) =>
+            {
+                Console.WriteLine(args.Exception);
+            };
+
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "FlexHeightShrink";
+        }
+
+        public override NSWindow MainWindow => _window;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+            global::Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new FlexHeightShrink.App());
+
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+
+        public override bool ApplicationShouldHandleReopen(NSApplication sender, bool hasVisibleWindows)
+        {
+            if (!hasVisibleWindows && _window != null)
+            {
+                _window.IsVisible = true;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ FlexHeightShrink/FlexHeightShrinkPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+namespace FlexHeightShrink
+{
+    public partial class FlexHeightShrinkPage : ContentPage
+    {
+        public FlexHeightShrinkPage()
+        {
+            InitializeComponent();
+
+            var flexRoot = new FlexLayout
+            {
+                Direction = FlexDirection.Column
+            };
+
+            var upperFlex = new FlexLayout
+            {
+                BackgroundColor = Color.Red
+            };
+            upperFlex.Children.Add(new Label
+            {
+                Text = "Upper"
+            });
+            FlexLayout.SetGrow(upperFlex, 1);
+
+            flexRoot.Children.Add(upperFlex);
+
+            var bottomFlex = new FlexLayout
+            {
+                BackgroundColor = Color.Yellow
+            };
+            bottomFlex.Children.Add(new Label
+            {
+                Text = "Bottom"
+            });
+            FlexLayout.SetBasis(bottomFlex, 100);
+
+            flexRoot.Children.Add(bottomFlex);
+
+            Content = flexRoot;
+        }
+    }
+}
